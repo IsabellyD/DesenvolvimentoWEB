01d98f93bf00df4550a0403f8380a367e0435b11
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-05-16 15:25:57 -0300

    Add files via upload
    Três camadas

---
+++ PrinterService.java
@@ -0,0 +1,6 @@
+public class PrinterService {
+
+    public void print(String mensagem) {
+        System.out.println(mensagem);
+    }
+}
---
+++ ReaderService.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReaderService {
+
+    private Scanner sc = new Scanner(System.in);
+
+    public int lerInt() {
+        return sc.nextInt();
+    }
+
+    public String lerString() {
+        sc.nextLine();
+        return sc.nextLine();
+    }
+
+    public String lerTexto() {
+        return sc.nextLine();
+    }
+}
