cfd3a01f7c9fca8ca3b0ff340c08238dace95934
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-05-27 16:22:11 -0300

    Add methods to read integers and strings from input

--- ReaderService.java
+++ ReaderService.java
@@ -5,15 +5,19 @@ public class ReaderService {
     private Scanner sc = new Scanner(System.in);
 
     public int lerInt() {
+
         return sc.nextInt();
     }
 
     public String lerString() {
+
         sc.nextLine();
+
         return sc.nextLine();
     }
 
     public String lerTexto() {
+
         return sc.nextLine();
     }
 }
