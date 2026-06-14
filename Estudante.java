be1d11a8b7953ed2cf8ada2aa66c2dd2b522c94c
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-06-14 17:22:52 -0300

    Refactor Estudante class with JPA annotations

--- Estudante.java
+++ Estudante.java
@@ -1,58 +1,52 @@
+package br.edu.ifpb.isabelly.projetoweb.model.entity;
 import java.util.ArrayList;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Estudante {
-
-    private int id;
-    private String nome;
-    private int idade;
-    private String matricula;
-
-    private ArrayList<Disciplina> disciplinas = new ArrayList<>();
-
-    public Estudante(int id, String nome,int idade, String matricula) {
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-        this.matricula = matricula;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public ArrayList<Disciplina> getDisciplinas() {
-        return disciplinas;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    public void adicionarDisciplina(Disciplina d) {
-
-
-        if (!disciplinas.contains(d)) {
-            disciplinas.add(d);
-        }
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private int idade;
+	private String matricula;
+	private ArrayList<Disciplina> disciplinas = new ArrayList<>();
+	public Estudante() {
+	}
+	public Estudante(String nome, int idade, String matricula) {
+		this.nome = nome;
+		this.idade = idade;
+		this.matricula = matricula;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public ArrayList<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public void adicionarDisciplina(Disciplina d) {
+		if (!disciplinas.contains(d)) {
+			disciplinas.add(d);
+		}
+	}
 }
