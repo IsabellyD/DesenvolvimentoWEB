775ed4e9d8344e443410cce9057e93ab20d03956
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-07-16 14:48:07 -0300

    Add findByNome method to DisciplinaRepository

--- DisciplinaRepository.java
+++ DisciplinaRepository.java
@@ -1,5 +1,10 @@
 package br.edu.ifpb.isabelly.projetoweb.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import br.edu.ifpb.isabelly.projetoweb.model.entity.Disciplina;
+
+@Repository
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+	Disciplina findByNome(String nome);
 }
