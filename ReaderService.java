54628d97078b9961e6870e1eeed5a6f0c1b2be78
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-07-16 14:40:55 -0300

    Refactor ReaderService to initialize Scanner in constructor

--- ReaderService.java
+++ ReaderService.java
@@ -1,23 +1,27 @@
+package br.edu.ifpb.isabelly.projetoweb.business.service;
+
 import java.util.Scanner;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReaderService {
+	private final Scanner sc;
 
-    private Scanner sc = new Scanner(System.in);
+	public ReaderService() {
+		super();
+		this.sc = new Scanner(System.in);
+	}
 
-    public int lerInt() {
+	public int lerInt() {
+		return sc.nextInt();
+	}
 
-        return sc.nextInt();
-    }
+	public String lerString() {
+		sc.nextLine();
+		return sc.nextLine();
+	}
 
-    public String lerString() {
-
-        sc.nextLine();
-
-        return sc.nextLine();
-    }
-
-    public String lerTexto() {
-
-        return sc.nextLine();
-    }
+	public String lerTexto() {
+		return sc.nextLine();
+	}
 }
