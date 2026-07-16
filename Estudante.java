d8123e7c6fd192a7c3356a3d0db1e8637aca76b9
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-07-16 14:45:07 -0300

    Refactor Estudante class to use List for disciplinas

--- Estudante.java
+++ Estudante.java
@@ -1,52 +1,75 @@
 package br.edu.ifpb.isabelly.projetoweb.model.entity;
+
 import java.util.ArrayList;
+import java.util.List;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
 @Entity
 public class Estudante {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String nome;
 	private int idade;
 	private String matricula;
-	private ArrayList<Disciplina> disciplinas = new ArrayList<>();
+
+	@ManyToMany(mappedBy = "estudantes", fetch = FetchType.EAGER)
+	private List<Disciplina> disciplinas;
+
 	public Estudante() {
+		super();
+		this.disciplinas = new ArrayList<>();
 	}
+
 	public Estudante(String nome, int idade, String matricula) {
+		super();
 		this.nome = nome;
 		this.idade = idade;
 		this.matricula = matricula;
+		this.disciplinas = new ArrayList<>();
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public String getMatricula() {
 		return matricula;
 	}
-	public ArrayList<Disciplina> getDisciplinas() {
+
+	public List<Disciplina> getDisciplinas() {
 		return disciplinas;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
 	}
-	public void adicionarDisciplina(Disciplina d) {
-		if (!disciplinas.contains(d)) {
-			disciplinas.add(d);
+
+	public void adicionarDisciplina(Disciplina disciplina) {
+		if (!disciplinas.contains(disciplina)) {
+			disciplinas.add(disciplina);
 		}
 	}
 }
