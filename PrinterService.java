c17de5b1ff3e450f7d23c235465e3d67085cde5a
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-07-16 14:40:01 -0300

    Refactor PrinterService to include Spring annotations

--- PrinterService.java
+++ PrinterService.java
@@ -1,7 +1,10 @@
+package br.edu.ifpb.isabelly.projetoweb.business.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class PrinterService {
-
-    public void print(String mensagem) {
-
-        System.out.println(mensagem);
-    }
+	public void print(String mensagem) {
+		System.out.println(mensagem);
+	}
 }
