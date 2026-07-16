ff66de6be8467da94ceda88aa16ab440b6d238d6
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-07-16 14:48:47 -0300

    Add findByNome method to EstudanteRepository

--- EstudanteRepository.java
+++ EstudanteRepository.java
@@ -1,6 +1,10 @@
 package br.edu.ifpb.isabelly.projetoweb.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.ifpb.isabelly.projetoweb.model.entity.Estudante;
-public interface EstudanteRepository extends JpaRepository<Estudante, Long> {
-}
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.edu.ifpb.isabelly.projetoweb.model.entity.Estudante;
+
+@Repository
+public interface EstudanteRepository extends JpaRepository<Estudante, Long> {
+	Estudante findByNome(String nome);
+}
