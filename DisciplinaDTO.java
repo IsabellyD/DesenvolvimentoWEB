2e4087db8b516f4507f59ddf3de709d63ff562b1
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-07-16 14:35:37 -0300

    Refactor DisciplinaDTO to use Long for id

--- DisciplinaDTO.java
+++ DisciplinaDTO.java
@@ -1,36 +1,51 @@
+package br.edu.ifpb.isabelly.projetoweb.business.dto;
+
 public class DisciplinaDTO {
 
-    private int id;
-    private String nome;
-    private String professor;
-    private int cargaHoraria;
+	private Long id;
+	private String nome;
+	private String professor;
+	private int cargaHoraria;
 
-    public DisciplinaDTO(int id, String nome, String professor, int cargaHoraria) {
-        this.id = id;
-        this.nome = nome;
-        this.professor = professor;
-        this.cargaHoraria = cargaHoraria;
-    }
+	public DisciplinaDTO(Long id, String nome, String professor, int cargaHoraria) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.professor = professor;
+		this.cargaHoraria = cargaHoraria;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getProfessor() {
-        return professor;
-    }
+	public String getProfessor() {
+		return professor;
+	}
 
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
 
-    @Override
-    public String toString() {
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-        return "ID: " + id + " | Disciplina: " + nome + " | Professor: " + professor + " | Carga Horária: " + cargaHoraria + "h";
-    }
+	public void setProfessor(String professor) {
+		this.professor = professor;
+	}
+
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + id + " | Disciplina: " + nome + " | Professor: " + professor + " | Carga Horária: "
+				+ cargaHoraria + "h";
+	}
 }
