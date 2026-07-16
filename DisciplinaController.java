d8ca4c9c24f9f8505f6ac3cb9bb958c810b0312f
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-07-16 14:46:00 -0300

    Refactor DisciplinaController with Spring annotations

--- DisciplinaController.java
+++ DisciplinaController.java
@@ -1,27 +1,34 @@
+package br.edu.ifpb.isabelly.projetoweb.presentation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import br.edu.ifpb.isabelly.projetoweb.business.dto.DisciplinaDTO;
+import br.edu.ifpb.isabelly.projetoweb.business.service.DisciplinaService;
+import br.edu.ifpb.isabelly.projetoweb.business.service.PrinterService;
+
+@Controller
 public class DisciplinaController {
+	private final DisciplinaService service;
 
-    private DisciplinaService service;
+	@Autowired
+	public DisciplinaController(DisciplinaService service) {
+		super();
+		this.service = service;
+	}
 
-    public DisciplinaController(DisciplinaService service) {
-        this.service = service;
-    }
+	public void cadastrar(DisciplinaDTO dto) {
+		service.cadastrar(dto);
+	}
 
-    public void cadastrar(DisciplinaDTO dto) {
-        service.cadastrar(dto);
-    }
+	public void editar(Long id, DisciplinaDTO dto) {
+		service.editar(id, dto);
+	}
 
-    public void editar(
-            int id,
-            DisciplinaDTO dto) {
+	public void listar(PrinterService printer) {
+		service.listar(printer);
+	}
 
-        service.editar(id, dto);
-    }
-
-    public void listar(PrinterService printer) {
-        service.listar(printer);
-    }
-
-    public void listarSimples(PrinterService printer) {
-        service.listarSimples(printer);
-    }
+	public void listarSimples(PrinterService printer) {
+		service.listarSimples(printer);
+	}
 }
