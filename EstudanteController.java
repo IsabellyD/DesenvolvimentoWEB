9844668f4442968bf62c0fa7bb333b0376897d80
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2026-07-16 14:47:00 -0300

    Refactor EstudanteController with Spring annotations

--- EstudanteController.java
+++ EstudanteController.java
@@ -1,31 +1,34 @@
+package br.edu.ifpb.isabelly.projetoweb.presentation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import br.edu.ifpb.isabelly.projetoweb.business.dto.EstudanteDTO;
+import br.edu.ifpb.isabelly.projetoweb.business.service.EstudanteService;
+import br.edu.ifpb.isabelly.projetoweb.business.service.PrinterService;
+
+@Controller
 public class EstudanteController {
+	private final EstudanteService service;
 
-    private EstudanteService service;
+	@Autowired
+	public EstudanteController(EstudanteService service) {
+		super();
+		this.service = service;
+	}
 
-    public EstudanteController(EstudanteService service) {
-        this.service = service;
-    }
+	public void cadastrar(EstudanteDTO dto) {
+		service.cadastrar(dto);
+	}
 
-    public void cadastrar(EstudanteDTO dto) {
-        service.cadastrar(dto);
-    }
+	public void editar(Long id, EstudanteDTO dto) {
+		service.editar(id, dto);
+	}
 
-    public void editar(
-            int id,
-            EstudanteDTO dto) {
+	public void listar(PrinterService printer) {
+		service.listar(printer);
+	}
 
-        service.editar(id, dto);
-    }
-
-    public void listar(
-            PrinterService printer) {
-
-        service.listar(printer);
-    }
-
-    public void listarSimples(
-            PrinterService printer) {
-
-        service.listarSimples(printer);
-    }
+	public void listarSimples(PrinterService printer) {
+		service.listarSimples(printer);
+	}
 }
